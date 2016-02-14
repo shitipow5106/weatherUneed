dc3ed1ddf6f58230709c51b326084d09b6cf665c
shiti_pow5106 <545606962@qq.com>
2016-02-14 20:56:24 +0800

    Second Commit

---
+++ src/com/weacheruneed/app/db/WeatherUneedHelper.java
@@ -0,0 +1,54 @@
+package com.weacheruneed.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class WeatherUneedHelper extends SQLiteOpenHelper {
+
+	public WeatherUneedHelper(Context context, String name,
+			CursorFactory factory, int version){
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	//province表建表语句
+	
+	public static final String CREATE_PROVINCE = "create table Province("
+			+"id integer primary key autoincrement,"
+			+"province_name text,"
+			+"province_code)";
+	
+	//city表建表语句
+	
+	public static final String CREATE_CITY = "create table City("
+			+"id integer primary key autoincrement,"
+			+"city_name text,"
+			+"city_code,"
+			+"province_id integer)";
+	
+	//country表建表语句
+	
+	public static final String CREATE_COUNTRY = "create table Country("
+			+"id integer primary key autoincrement,"
+			+"country_name text,"
+			+"country_code,"
+			+"city_id integer)";
+	
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(CREATE_PROVINCE);
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTRY);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
